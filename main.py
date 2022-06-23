0384cece8df85ea93c20fb2d022bbe57afd2c376
Dmitriy <dmitriy.tolstyy@mail.ru>
2022-06-23 15:19:23 +0300

    password

---
+++ main.py
@@ -0,0 +1,16 @@
+print('PASSWORD')
+
+
+def code():
+    name = input('Введите имя: ')
+    p = input('Введите пароль: ')
+    if len(p) <= 8:
+        print('Введите пароль больше 8 символов.')
+        code()
+    elif set(name) <= set(p):
+        print('Пароль  не  должен содержать имя')
+        code()
+
+code()
+print('OK PASSWORD')
+
